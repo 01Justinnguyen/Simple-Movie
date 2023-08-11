24e0dfd3af386d66a82ce4e192b78ed69f77b251
Liam <phuc01112002@gmail.com>
2023-08-11 15:12:40 +0700

    add loading skeleton and pagination UI no features

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,27 @@ import HomePage from './pages/HomePage'
 import Banner from './components/banner/BannerList'
 import MoviesPage from './pages/MoviesPage'
 import MovieDetailPage from './components/movie/MovieDetailPage'
+import 'react-loading-skeleton/dist/skeleton.css'
+import { SkeletonTheme } from 'react-loading-skeleton'
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Main />}>
-          <Route
-            path="/"
-            element={
-              <>
-                <Banner />
-                <HomePage />
-              </>
-            }></Route>
-          <Route path="/movies" element={<MoviesPage />}></Route>
-          <Route path="/movie/:movieId" element={<MovieDetailPage />}></Route>
-        </Route>
-      </Routes>
+      <SkeletonTheme baseColor="#313131" highlightColor="#525252">
+        <Routes>
+          <Route path="/" element={<Main />}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner />
+                  <HomePage />
+                </>
+              }></Route>
+            <Route path="/movies" element={<MoviesPage />}></Route>
+            <Route path="/movie/:movieId" element={<MovieDetailPage />}></Route>
+          </Route>
+        </Routes>
+      </SkeletonTheme>
     </>
   )
 }
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -5,12 +5,14 @@ import { fetcher, API_KEY } from '../config'
 import MovieCard from '../components/movie/MovieCard'
 import debounce from 'lodash.debounce'
 const MoviesPage = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const [movies, setMovies] = useState([])
   const [inputValue, setInputValue] = useState('')
   const [url, setUrl] = useState(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
   const { data } = useSWR(url, fetcher)
   useEffect(() => {
     if (data && data.results) setMovies(data?.results)
+    // setIsLoading(false)
   }, [data])
   const handleInputChange = (e) => setInputValue(e.target.value)
   const setFilterDebounce = debounce(handleInputChange, 500)
@@ -33,7 +35,20 @@ const MoviesPage = () => {
           </svg>
         </button>
       </div>
-      <div className="grid grid-cols-4 gap-10">{movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} item={movie}></MovieCard>)}</div>
+      <div className="grid grid-cols-4 gap-10">{movies.length > 0 && movies.map((movie) => <MovieCard isLoading={isLoading} key={movie.id} item={movie}></MovieCard>)}</div>
+      <div className="flex items-center justify-center mt-10 gap-x-5">
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+        </span>
+        <span className="inline-block px-3 py-2 leading-none bg-white rounded cursor-pointer text-slate-900">1</span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </span>
+      </div>
     </div>
   )
 }
